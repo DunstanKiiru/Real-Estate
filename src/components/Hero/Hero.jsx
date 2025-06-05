da1903701f3c363da189196630105b7f397d1939
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-05 22:40:23 +0300

    Adds stats div to herosection

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,18 +8,25 @@ const Hero = () => {
       <div className="paddings innerWidth flexCenter hero-container">
         {/*left*/}
         <div className="flexColStart hero-left">
+
             <div className="hero-title">
                 <div className="orange-circle"/>
                 <h1>Discover<br/>Most Suitable<br/>Property</h1>
             </div>
+            
             <div className="flexColStart hero-des">
                 <span>Find a variety of properties that suit you very easily</span>
                 <span> Forget all difficulties in finding a residence for you</span>
             </div>
-            <div className="search-bar">
+
+            <div className=" flexCenter search-bar">
                 <HiLocationMarker color="var(--blue)" size={25} />
                 <input type="text" />
                 <button className="button">Search</button>
+            </div>
+
+            <div className="flexCenter stats">
+              
             </div>
         </div>
 
