d3f82e77ade792145b7803e71b0938ff7c58c298
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-05 15:28:36 +0300

    changes files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import './Header.css'
 
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="h-container">
-        <img src="./kenya-nannies logo3.png" alt="logo" width={100} />
+      <div className="flexCenter paddings innerWidth h-container">
+        <img src="/kenya-nannies logo3.png" alt="logo" width={100} />
 
         <div className="h-menu">
           <a href=""> Home</a>
