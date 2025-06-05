9062f2f8d30477effca55e112133057217a6351b
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-05 21:15:17 +0300

    Completes header Component and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="flexCenter paddings innerWidth h-container">
         <img src="/kenya-nannies logo3.png" alt="logo" width={100} />
 
-        <div className="h-menu">
+        <div className=" flexCenter h-menu">
           <a href=""> Home</a>
           <a href="">Our Value</a>
           <a href="">Contact Us</a>
