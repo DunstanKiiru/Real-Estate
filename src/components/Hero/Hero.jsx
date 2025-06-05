a133ff020020c981c16afe770c7822aee148fe9d
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-05 22:19:25 +0300

    adds searchbar to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import  "./Hero.css"
+import {HiLocationMarker} from  'react-icons/hi'
 
 const Hero = () => {
   return (
@@ -16,7 +17,9 @@ const Hero = () => {
                 <span> Forget all difficulties in finding a residence for you</span>
             </div>
             <div className="search-bar">
-                    Search Bar
+                <HiLocationMarker color="var(--blue)" size={25} />
+                <input type="text" />
+                <button className="button">Search</button>
             </div>
         </div>
 
