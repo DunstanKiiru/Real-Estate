fa51112d104c4494b550238018594918844323aa
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-06 14:11:51 +0300

    configures countup to stats

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,33 +9,53 @@ const Hero = () => {
       <div className="paddings innerWidth flexCenter hero-container">
         {/*left*/}
         <div className="flexColStart hero-left">
-            <div className="hero-title">
-                <div className="orange-circle"/>
-                <h1>Discover<br/>Most Suitable<br/>Property</h1>
-            </div>
-            
-            <div className="flexColStart hero-des">
-                <span>Find a variety of properties that suit you very easily</span>
-                <span> Forget all difficulties in finding a residence for you</span>
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover
+              <br />
+              Most Suitable
+              <br />
+              Property
+            </h1>
+          </div>
+
+          <div className="flexColStart hero-des">
+            <span>Find a variety of properties that suit you very easily</span>
+            <span> Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className=" flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+
+          <div className="flexCenter stats">
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={8000} end={10000} duration={6} />
+                <span>+</span>
+              </span>
+              <span>premium products</span>
             </div>
 
-            <div className=" flexCenter search-bar">
-                <HiLocationMarker color="var(--blue)" size={25} />
-                <input type="text" />
-                <button className="button">Search</button>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={1900} end={2000} duration={6} />
+                <span>+</span>
+              </span>
+              <span>Happy Customers</span>
             </div>
 
-            <div className="flexCenter stats">
-              <div className="flexColStart stat">
-                <span>
-                  <CountUp start={8000} end={10000} duration={6} />
-                  <span>+</span>
-                </span>
-                <span>
-                  premium products
-                </span>
-              </div>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp end={28}/>
+                <span>+</span>
+              </span>
+              <span>Award Winnings</span>
             </div>
+          </div>
         </div>
 
         {/*right*/}
