ca8638e29969d1716b5a0a1890f9d576a742ec2f
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-09 01:08:28 +0300

    Updates Contact.jsx and css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Contact.css";
+import {MdCall} from 'react-icons/md'
+import {BsFillChatDotsFill} from 'react-icons/bs'
+import {HiChatBubbleBottomCenter} from 'react-icons/hi2'
 
 const Contact = () => {
   return (
@@ -10,8 +13,29 @@ const Contact = () => {
             <span className="orangeText">Our Contacts</span>
             <span className="primaryText">Easy to Contact us</span>
             <span className="secondaryText">We always ready to help by providing the best services for you.
-            We believe a good place to live can make your life better
-            </span>
+            We believe a good place to live can make your life better</span>
+
+            <div className="flexColStart contactModes">
+                {/*first row*/}
+                <div className="flexColStart row">
+                    <div className="flexColCenter mode">
+                        <div className="flexStart">
+                            <div className="flexCenter icon">
+                                <MdCall size={25}/>
+                            </div>
+                            <div className="flexColStart detail">
+                                <span>Call</span>
+                                <span>0720206198</span>
+                            </div>
+                        </div>
+
+                        <div className="flexCenter button">
+                            CallNow
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
         {/*right*/}
         <div className="c-right">
