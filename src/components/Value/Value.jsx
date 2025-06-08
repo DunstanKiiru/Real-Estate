dae9a384bce287b62cfb5864c60ca044ff558be8
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-08 23:22:23 +0300

    Updates value.css

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -19,6 +19,15 @@ const Value = () => {
                     <img src="./value.png" alt="" />
                 </div>
             </div>
+            {/*right */}
+            <div className="flexColStart v-right">
+                <span className='orangeText'>Our Value</span>
+                <span className='primaryText'>Value We Give to You</span>
+                <span className='secondaryText'>We always ready to help by providing the best services for you.
+                <br />
+                We believe a good place to live can make your life better.
+                </span>
+            </div>
         </div>
     </section>
   )
