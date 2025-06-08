f20273be18cb9e93982d5057fbcf2aeecc57c2d5
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-09 01:40:10 +0300

    Updates contact.jsx and css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -16,10 +16,11 @@ const Contact = () => {
             We always ready to help by providing the best services for you. We
             believe a good place to live can make your life better
           </span>
-
+          
+          {/*first mode*/}
           <div className="flexColStart contactModes">
             {/*first row*/}
-            <div className="flexColStart row">
+            <div className="flexStart row">
               <div className="flexColCenter mode">
                 <div className="flexStart">
                   <div className="flexCenter icon">
@@ -34,7 +35,7 @@ const Contact = () => {
                 <div className="flexCenter button">Call now</div>
               </div>
 
-              {/*second row*/}
+              {/*second mode*/}
               <div className="flexColCenter mode">
                 <div className="flexStart">
                   <div className="flexCenter icon">
@@ -47,6 +48,38 @@ const Contact = () => {
                 </div>
 
                 <div className="flexCenter button">Chat now</div>
+              </div>
+            </div>
+
+            {/*second row*/}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Video Call</span>
+                    <span className="secondaryText">0720206198</span>
+                  </div>
+                </div>
+
+                <div className="flexCenter button">Video Call now</div>
+              </div>
+
+              {/*second mode*/}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Message</span>
+                    <span className="secondaryText">0720206198</span>
+                  </div>
+                </div>
+
+                <div className="flexCenter button">Message now</div>
               </div>
             </div>
           </div>
