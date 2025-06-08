30e30b557e6f2f3da41c4d1d264f8450eb270b26
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-08 23:03:21 +0300

    Updates value.jsx

---
+++ src/components/Value/Value.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import{
+    Accordion,
+    AccordionItem,
+    AccordioItemHeading,
+    AccordionItemPanel,
+    AccordionItemState,
+} from 'react-accessible-accordion'
+const Value = () => {
+  return (
+    <div>Value</div>
+  )
+}
+
+export default Value
